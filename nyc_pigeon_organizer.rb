a59fff5234991da21545428a8ea9b4e19b702171
Emily Haynes <ehayne00@hotmail.com>
2019-10-21 04:44:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,6 +2,6 @@ def nyc_pigeon_organizer(data)
   new_pigeon_hash = {}
 data.each do |key, value|
   value.each do |stats, pigeon_names|
-    pigeon_names.each do 
+    pigeon_names.each do |name|
 end
 end
